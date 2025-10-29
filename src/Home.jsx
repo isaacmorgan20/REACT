c0fb7862f6d071909d6da8c38435f9a9bd261790
Isaac-Morgan <morganisaackojo5547@gmail.com>
2025-10-29 14:13:06 +0000

    hi

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='bg-green-400 text-blue-400'>
+    <div className='bg-green-400 text-blue-700'>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil cupiditate voluptas praesentium, quis iusto rem consectetur ullam eos blanditiis nulla. Accusamus id enim sunt voluptate esse, recusandae eum soluta sit?</p>
         <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur voluptatum, in, quae et, totam sapiente fuga molestias rem provident eos beatae aut! At expedita, suscipit voluptatem animi rem eos harum?</h1>
     </div>
