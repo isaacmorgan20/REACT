9da06fad715a9cec9cca94e4adc8b1a7dee1eac7
Isaac-Morgan <morganisaackojo5547@gmail.com>
2025-10-29 14:06:51 +0000

    react exercise

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='bg-green-400 text-blue-400'>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil cupiditate voluptas praesentium, quis iusto rem consectetur ullam eos blanditiis nulla. Accusamus id enim sunt voluptate esse, recusandae eum soluta sit?</p>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur voluptatum, in, quae et, totam sapiente fuga molestias rem provident eos beatae aut! At expedita, suscipit voluptatem animi rem eos harum?</h1>
+    </div>
+  )
+}
+
+export default Home
