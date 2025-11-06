208259d29ead88fdec15fed732dacfddbf9b6855
Isaac-Morgan <morganisaackojo5547@gmail.com>
2025-11-06 14:52:47 +0000

    React week 2 Assignment

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Card from './card'
+
+const App = () => {
+  return (
+    <Card />
+  )
+}
+
+export default App
---
+++ src/Card.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    
+    <div>
+        <h1 className='text-center font-bold text-2xl underline pt-4'>These are images of animals</h1>
+        <div className='grid grid-cols-3 gap-4 p-10 '>
+        {/* one */}
+        <div className='bg-blue-400 rounded-lg h-250 w-100 p-3 shadow-lg'>
+                  <p className='bg-yellow-400 text-xl text-center p-2 rounded-t-lg font-bold'>This is a  Giraffe</p>
+            <img src="https://images.pexels.com/photos/34098/south-africa-hluhluwe-giraffes-pattern.jpg" alt="" className='rounded-b-lg pb-4 ' />
+            <p>The <b className='text-white'>giraffe</b> is a large African hoofed mammal belonging to the genus <b className='text-white'>Giraffa</b>. It is the tallest living terrestrial animal and the largest ruminant on Earth. It is classified under the family <b className='text-white'>Giraffidae</b>, along with its closest extant relative, the okapi. Traditionally, <b className='text-white'>giraffes</b> have been thought of as one species, <b className='text-white'>Giraffa</b> camelopardalis, with nine subspecies. Most recently, researchers proposed dividing them into four extant species, with seven subspecies, which can be distinguished morphologically by their fur coat patterns. Six valid extinct species of <b className='text-white'>Giraffa</b> are known from the fossil record.</p>
+            <button className='bg-yellow-300 rounded-lg px-4 p-2 mt-2 shadow-lg hover:shadow-xl hover:bg-yellow-500 hover:font-bold'>Learn more</button>
+            </div> 
+            {/* two */}
+        <div className='bg-blue-400 rounded-lg h-250 w-100 p-3 shadow-lg'>
+            <p className='bg-yellow-400 text-xl text-center p-2 rounded-t-lg font-bold'>This is a tiger</p>
+            <img src="https://images.pexels.com/photos/162203/panthera-tigris-altaica-tiger-siberian-amurtiger-162203.jpeg" alt="" className='rounded-b-lg pb-4'/>
+            <p><b className='text-xl'>Tigers</b> are a critical part of ecosystems and cultures. If forests are emptied of every last tiger, all that will remain are distant legends and zoo sightings.<b>Tigers</b> have evolved over millions of years. Currently, they're being trapped, their parts trafficked for various purposes, and they're getting pushed out of their homes. Many that are left in the wild cling to survival in isolated patches of forest scattered across Asia.To save <b>tigers</b>, we need to secure forest and grassland habitats across Asia where they live. we allow tigers to roam and preserve the many other threatened species that live there.</p>
+             <button className='bg-yellow-300 rounded-lg px-4 p-2 mt-2 shadow-lg hover:shadow-xl hover:bg-yellow-500 hover:font-bold'>Learn more</button>
+            </div> 
+            {/* three */}
+        <div className='bg-blue-400 rounded-lg h-250 w-100 p-3 shadow-lg'>
+            <p className='bg-yellow-400 text-xl text-center p-2 rounded-t-lg font-bold'>This is a fox</p>
+            <img src="https://images.pexels.com/photos/2295744/pexels-photo-2295744.jpeg" alt="" />
+            <p className='pt-7'><b className='text-xl'>Foxes</b> are small-to-medium-sized omnivorous mammals belonging to several genera of the family Canidae. They have a flattened skull; upright, triangular ears; a pointed, slightly upturned snout; and a long, bushy tail ("brush").Twelve species belong to the monophyletic "true fox" group of genus Vulpes. Another 25 current or extinct species are sometimes called <b>foxes</b> – they are part of the paraphyletic group of the South American <b>foxes</b> or an outlying group, which consists of the bat-eared fox, gray fox, and island fox.[1]</p>
+            <button className='bg-yellow-300 rounded-lg px-4 p-2 mt-7 shadow-lg hover:shadow-xl hover:bg-yellow-500 hover:font-bold'>Learn more</button>
+            </div> 
+            {/* four */}
+        <div className='bg-blue-400 rounded-lg h-280 w-100 p-3 shadow-lg'>
+             <p className='bg-yellow-400 text-xl text-center p-2 rounded-t-lg font-bold'>This is a Cock</p>
+            <img src="https://images.pexels.com/photos/1405930/pexels-photo-1405930.jpeg" alt="" />
+            <p className='pt-7'>The chicken (Gallus gallus domesticus) is a domesticated subspecies of the red junglefowl (Gallus gallus), originally native to Southeast Asia. It was first domesticated around 8,000 years ago and is one of the most common and widespread domesticated animals in the world. Chickens are primarily kept for their meat and eggs, though they are also kept as pets.[1]
+            As of 2023, the global chicken population exceeds 26.5 billion, with more than 50 billion birds produced annually for consumption. Specialized breeds such as broilers and laying hens have been developed for meat and egg production, respectively. A hen bred for laying can produce over 300 eggs per year. Chickens are social animals with complex vocalizations and behaviors, and feature prominently in folklore, religion, and literature across many societies.</p>
+         <button className='bg-yellow-300 rounded-lg px-4 p-2 mt-1 shadow-lg hover:shadow-xl hover:bg-yellow-500 hover:font-bold'>Learn more</button> 
+        </div>
+        {/* five */}
+        <div className='bg-blue-400 rounded-lg h-280 w-100 p-3 shadow-lg'>
+             <p className='bg-yellow-400 text-xl text-center p-2 rounded-t-lg font-bold'>This is a horse</p>
+            <img src="https://images.pexels.com/photos/2313396/pexels-photo-2313396.jpeg" alt="Horse" />
+            <p className='pt-7'>The horse (Equus ferus caballus)[2][5] is a domesticated, one-toed, hoofed mammal. It belongs to the taxonomic family Equidae and is one of two extant subspecies of Equus ferus. The horse has evolved over the past 45 to 55 million years from a small multi-toed creature, Eohippus, into the large, single-toed animal of today. Humans began domesticating horses around 4000 BCE in Central Asia, and their domestication is believed to have been widespread by 3000 BCE. Horses in the subspecies caballus are domesticated, although some domesticated populations live in the wild as feral horses. These feral populations are not true wild horses, which are horses that have never been domesticated.</p>
+         <button className='bg-yellow-300 rounded-lg px-4 p-2 mt-1 shadow-lg hover:shadow-xl hover:bg-yellow-500 hover:font-bold'>Learn more</button>
+         </div>
+         {/* six */}
+        <div className='bg-blue-400 rounded-lg h-250 w-100 p-3 shadow-lg'>
+             <p className='bg-yellow-400 text-xl text-center p-2 rounded-t-lg font-bold'>This is an elephant</p>
+            <img src="https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg" alt="" />
+            <p className='pt-7'>Elephants are the largest living land animals. Three living species are currently recognised: the African bush elephant (Loxodonta africana), the African forest elephant (L. cyclotis), and the Asian elephant (Elephas maximus). They are the only surviving members of the family Elephantidae and the order Proboscidea; extinct relatives include mammoths and mastodons. Distinctive features of elephants include a long proboscis called a trunk, tusks, large ear flaps, pillar-like legs, and tough but sensitive grey skin. The trunk is prehensile, bringing food and water to the mouth and grasping objects. Tusks, which are derived from the incisor teeth, serve both as weapons and as tools for moving objects and digging. The large ear flaps assist in maintaining a constant body temperature as well as in communication. African elephants have larger ears and concave backs, whereas Asian elephants have smaller ears and convex or level backs.</p>
+         <button className='bg-yellow-300 rounded-lg px-4 p-2 mt-1 shadow-lg hover:shadow-xl hover:bg-yellow-500 hover:font-bold'>Learn more</button>
+         </div>
+        
+        </div>
+   </div>
+  )
+}
+
+export default Card
